e7b7358f99e096df037bbd7393395e9ed7a1899f
Ben Kanagroo <78645337+Lenniott@users.noreply.github.com>
2024-11-16 16:36:51 +0000

    supabase fixes

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -7,6 +7,12 @@ interface Props {
 
 const Options: React.FC<Props> = ({ title }: Props) => {
   const [apiKey, setApiKey] = useState('');
+  const [supabaseUrl, setSupabaseUrl] = useState('');
+  const [supabaseKey, setSupabaseKey] = useState('');
+  const [localStorageData, setLocalStorageData] = useState<{
+    [key: string]: any;
+  }>({});
+
   const toggleApiKeyInput = (isApiKeyPresent: boolean) => {
     const apiKeyInput = document.getElementById('apiKeyInput');
     const saveApiKey = document.getElementById('saveApiKey');
@@ -23,20 +29,49 @@ const Options: React.FC<Props> = ({ title }: Props) => {
     }
   };
 
+  const toggleSupabaseInput = (isSupabasePresent: boolean) => {
+    const supabaseUrlInput = document.getElementById('supabaseUrlInput');
+    const supabaseKeyInput = document.getElementById('supabaseKeyInput');
+    const saveSupabaseKey = document.getElementById('saveSupabaseKey');
+    const removeSupabaseKey = document.getElementById('removeSupabaseKey');
+
+    if (supabaseUrlInput && supabaseKeyInput) {
+      supabaseUrlInput.style.display = isSupabasePresent ? 'none' : 'block';
+      supabaseKeyInput.style.display = isSupabasePresent ? 'none' : 'block';
+    }
+    if (saveSupabaseKey) {
+      saveSupabaseKey.style.display = isSupabasePresent ? 'none' : 'block';
+    }
+    if (removeSupabaseKey) {
+      removeSupabaseKey.style.display = isSupabasePresent ? 'block' : 'none';
+    }
+  };
+
   async function logKey() {
     try {
-      const { openaiApiKey } = await new Promise<{ openaiApiKey?: string }>(
-        (resolve) => {
-          chrome.storage.local.get(['openaiApiKey'], (result) =>
-            resolve(result)
-          );
-        }
-      );
+      const { openaiApiKey, supabaseUrl, supabaseKey } = await new Promise<{
+        openaiApiKey?: string;
+        supabaseUrl?: string;
+        supabaseKey?: string;
+      }>((resolve) => {
+        chrome.storage.local.get(
+          ['openaiApiKey', 'supabaseUrl', 'supabaseKey'],
+          (result) => resolve(result)
+        );
+      });
 
       if (!openaiApiKey) {
         setApiKey('Please set your OpenAI API key.');
       } else {
         setApiKey(openaiApiKey);
+      }
+
+      if (!supabaseUrl || !supabaseKey) {
+        setSupabaseUrl('Please set your Supabase URL and Key.');
+        setSupabaseKey('');
+      } else {
+        setSupabaseUrl(supabaseUrl);
+        setSupabaseKey(supabaseKey);
       }
     } catch (error) {
       console.error('Error:', error);
@@ -71,13 +106,64 @@ const Options: React.FC<Props> = ({ title }: Props) => {
     });
   };
 
-  useEffect(() => {
-    chrome.storage.local.get(['openaiApiKey'], (result) => {
-      toggleApiKeyInput(!!result.openaiApiKey);
+  const saveSupabaseCredentials = async () => {
+    const supabaseUrlInput = document.getElementById(
+      'supabaseUrlInput'
+    ) as HTMLInputElement;
+    const supabaseKeyInput = document.getElementById(
+      'supabaseKeyInput'
+    ) as HTMLInputElement;
+    const supabaseUrl = supabaseUrlInput.value;
+    const supabaseKey = supabaseKeyInput.value;
+    const responseElement = document.getElementById('response') as HTMLElement;
+
+    if (!supabaseUrl.trim() || !supabaseKey.trim()) {
+      responseElement.innerText = 'Please enter a valid Supabase URL and Key.';
+      return;
+    }
+    chrome.storage.local.set({ supabaseUrl, supabaseKey }, () => {
+      responseElement.innerText = 'Supabase URL and Key saved successfully!';
+      toggleSupabaseInput(true);
     });
+  };
+
+  const removeSupabaseCredentials = async () => {
+    chrome.storage.local.remove(['supabaseUrl', 'supabaseKey'], () => {
+      const responseElement = document.getElementById(
+        'response'
+      ) as HTMLElement;
+      responseElement.innerText = 'Supabase credentials removed successfully!';
+      toggleSupabaseInput(false);
+    });
+  };
+
+  const viewLocalStorage = async () => {
+    chrome.storage.local.get(null, (result) => {
+      setLocalStorageData(result);
+    });
+  };
+
+  const removeLocalStorageItem = async (key: string) => {
+    chrome.storage.local.remove(key, () => {
+      viewLocalStorage();
+    });
+  };
+
+  useEffect(() => {
+    chrome.storage.local.get(
+      ['openaiApiKey', 'supabaseUrl', 'supabaseKey'],
+      (result) => {
+        toggleApiKeyInput(!!result.openaiApiKey);
+        toggleSupabaseInput(!!result.supabaseUrl && !!result.supabaseKey);
+        setSupabaseUrl(result.supabaseUrl || '');
+        setSupabaseKey(result.supabaseKey || '');
+      }
+    );
 
     const saveButton = document.getElementById('saveApiKey');
     const removeButton = document.getElementById('removeApiKey');
+    const saveSupabaseButton = document.getElementById('saveSupabaseKey');
+    const removeSupabaseButton = document.getElementById('removeSupabaseKey');
 
     if (saveButton) {
       saveButton.addEventListener('click', saveApiKey);
@@ -85,6 +171,14 @@ const Options: React.FC<Props> = ({ title }: Props) => {
 
     if (removeButton) {
       removeButton.addEventListener('click', removeApiKey);
+    }
+
+    if (saveSupabaseButton) {
+      saveSupabaseButton.addEventListener('click', saveSupabaseCredentials);
+    }
+
+    if (removeSupabaseButton) {
+      removeSupabaseButton.addEventListener('click', removeSupabaseCredentials);
     }
   }, []);
 
@@ -100,12 +194,49 @@ const Options: React.FC<Props> = ({ title }: Props) => {
           />
           <button id="saveApiKey">Save API Key</button>
           <button id="removeApiKey">Remove API Key</button>
+
+          <input
+            type="text"
+            id="supabaseUrlInput"
+            placeholder="Enter your Supabase URL"
+          />
+          <input
+            type="text"
+            id="supabaseKeyInput"
+            placeholder="Enter your Supabase Key"
+          />
+          <button id="saveSupabaseKey">Save Supabase Credentials</button>
+          <button id="removeSupabaseKey">Remove Supabase Credentials</button>
         </div>
 
         <button id="logKey" onClick={logKey}>
           Log Key
         </button>
-        <div id="response">{apiKey}</div>
+        <button id="viewLocalStorage" onClick={viewLocalStorage}>
+          View Local Storage
+        </button>
+        <div id="response">
+          {apiKey}
+          <br />
+          {supabaseUrl}
+        </div>
+        <div className="localStorageView">
+          <h2>Local Storage Data:</h2>
+          <div className="container">
+            {Object.keys(localStorageData).map((key) => (
+              <div key={key} className="container row">
+                <button onClick={() => removeLocalStorageItem(key)}>
+                  Remove
+                </button>
+                <div className="container">
+                  {`${key}: ~ ${
+                    JSON.stringify(localStorageData[key]).length
+                  } kb`}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </main>
     </div>
   );
